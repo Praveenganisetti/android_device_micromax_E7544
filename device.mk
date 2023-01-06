c77d62d4275fe839d9a2588346b79c4f3349c96c
Abhijeet <98699436+gitclone-url@users.noreply.github.com>
2023-01-06 23:33:03 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # A/B
 TARGET_IS_VAB := true
+ENABLE_VIRTUAL_AB := true
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
